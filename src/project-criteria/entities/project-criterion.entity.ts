3fb3f291d868418b663e6284523745b4f448f1e5
guianderson <guilherme.anderson34@outlook.com>
2021-09-06 22:23:26 -0300

    initial configures of typeorm

--- src/project-criteria/entities/project-criterion.entity.ts
+++ src/project-criteria/entities/project-criterion.entity.ts
@@ -1,9 +1,26 @@
+import { Entity, Column, PrimaryColumn } from "typeorm";
+
+@Entity()
 export class ProjectCriterion {
+    
+    @PrimaryColumn({type:'uuid'})
     id_criterio     : number;
+
+    @Column({nullable:false})
     id_projeto      : number;
+
+    @Column({nullable:false})
     desc_criterio   : string;
+
+    @Column({nullable:false})
     nota_minima     : number;
+
+    @Column({nullable:false})
     nota_maxima     : number;
+
+    @Column({nullable:false})
     nota_peso       : number;
+
+    @Column({nullable:false})
     sn_ativo        : string;
 }
