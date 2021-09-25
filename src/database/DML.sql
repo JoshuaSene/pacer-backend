69c048eacd14fdf11a4c74bbb419a963d405e04f
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2021-09-25 11:49:46 -0300

    Acerto campo sn_ativo na entidade Criteria.

--- src/database/DML.sql
+++ src/database/DML.sql
@@ -2,10 +2,10 @@
 -- SET SQL_SAFE_UPDATES = 0;
 
 -- CRITERIA
-insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('1','Proatividade','N');
-insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('2','Autonomia','N');
-insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('3','Colaboração','N');
-insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('4','Entrega de Resultados','N');
+insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('1','Proatividade','S');
+insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('2','Autonomia','S');
+insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('3','Colaboração','S');
+insert into criteria (id_criteria,desc_criteria,sn_ativo) values ('4','Entrega de Resultados','S');
 commit;
 -- select * from criteria;
 
@@ -15,6 +15,12 @@ insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_cri
 insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_criteria,id_sprint) values (3,11,22,1,3,1);
 insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_criteria,id_sprint) values (4,11,22,1,4,1);
     
+insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_criteria,id_sprint,note) values (5,12,22,1,1,1,2);
+insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_criteria,id_sprint,note) values (6,12,22,1,2,1,3);
+insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_criteria,id_sprint) values (7,12,22,1,3,1);
+insert into notes_store (id_evaluation,id_evaluator,id_evaluated,id_group,id_criteria,id_sprint) values (8,12,22,1,4,1);
+
+
 select * from notes_store order by id_evaluation, id_evaluator;    
 
 delete from notes_store;
