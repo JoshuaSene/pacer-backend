d4a12c582d3cabef609237c9751deee4444dab8c
Jonathas H. Moraes <jonathas.moraes@fatec.sp.gov.br>
2021-09-30 19:52:08 -0300

    Finishing controller and service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 import { User } from "./entities/user.entity";
 
 @Injectable()
@@ -11,5 +13,30 @@ export class UserService {
         private repository: Repository<User>,
     ) {}
 
+    async create(createuserDto: CreateUserDto): Promise<User> {
+        const createdUser = await this.repository.create(
+            createuserDto
+        ); 
+        return await this.repository.save(createdUser);
+    }
 
+    async findAll(): Promise<User[]> {
+        return this.repository.find();
+    }
+
+    async findOne(id: string): Promise<User> {
+        return await this.repository.findOne(id);
+    }
+
+    async update(id: string, dto: UpdateUserDto): Promise<User> {
+        const user = await this.repository.findOne(id);
+        const merge = this.repository.merge(user, dto);
+        return await this.repository.save(merge);
+    }
+
+    async remove(id: string): Promise<string>  {
+        const user = await this.repository.findOne(id);
+        this.repository.delete(user.idUser);
+        return `User ${id} has been deleted`;
+    }
 }
